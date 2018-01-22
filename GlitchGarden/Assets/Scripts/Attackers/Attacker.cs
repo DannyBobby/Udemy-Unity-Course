548628f002c81c0fdc13e27b8bcddb0736c2b836
Danny Glidewell <drg8705@gmail.com>
2018-01-22 09:17:11 -0500

    Movement & Triggers
    Added movement to attackers and adjusted speeds so they look right.
    
    Added trigger colliders and rigidbody2D components where necessary.
    
    Animation events on attackers to control movement speed depending on state.
    
    Altered stated transitions for attackers.
    
    Beginning to flesh out specialized behavior for attackers.

---
+++ GlitchGarden/Assets/Scripts/Attackers/Attacker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(0f, 1.5f)] [SerializeField]
+    private float currentSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Attacker " + gameObject.name + " trigger enter " + collider.name);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    void StrikeCurrentTarget(float damage)
+    {
+        Debug.Log(name + " dealt " + damage.ToString() + " to target!");
+    }
+ }
